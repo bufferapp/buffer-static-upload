f046a3a67d6fdb9e5c036045525f1bab30fd03a1
Dan Farrelly <daniel.j.farrelly@gmail.com>
2017-06-15 18:11:15 -0400

    Add build script for releases

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Clean
+echo "Cleaning up"
+rm -rf dist >/dev/null 2>&1
+mkdir dist >/dev/null 2>&1
+rm buffer-static-upload >/dev/null 2>&1
+
+# Build native
+echo "Compiling native binary"
+go build main.go
+mv main buffer-static-upload
+
+# Build linux
+echo "Compiling linux binary"
+env GOOS=linux GOARCH=amd64 go build main.go
+mv main ./dist/buffer-static-upload-Linux
+
+# Build mac
+echo "Compiling mac binary"
+env GOOS=darwin GOARCH=amd64 go build main.go
+mv main ./dist/buffer-static-upload-Darwin
+
+echo "Done!"
