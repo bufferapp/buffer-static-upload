06150f34aa5f7c2d427d97dc0cb86c361cd9e338
Dan Farrelly <daniel.j.farrelly@gmail.com>
2017-07-06 16:18:04 -0400

    Reduce size of binary

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ mv main buffer-static-upload
 
 # Build linux
 echo "Compiling linux binary"
-env GOOS=linux GOARCH=amd64 go build main.go
+env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" main.go
 mv main ./dist/buffer-static-upload-Linux
 
 # Build mac
 echo "Compiling mac binary"
-env GOOS=darwin GOARCH=amd64 go build main.go
+env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" main.go
 mv main ./dist/buffer-static-upload-Darwin
 
 echo "Done!"
